c6ac29196c0328988277a96d2940c280d5bd4ba7
Jason Yao <jasonyao@midway3-login4.rcc.local>
2025-01-09 17:53:53 -0600

    keeping Threads, throwing away standards

--- cmake/RootFftwWrapperConfig.cmake
+++ cmake/RootFftwWrapperConfig.cmake
@@ -3,5 +3,6 @@ include("${CMAKE_CURRENT_LIST_DIR}/RootFftwWrapperTargets.cmake")
 include(CMakeFindDependencyMacro)
 # find_dependency(FFTW REQUIRED) # TODO CONFIG MODE???
 find_dependency(ROOT CONFIG REQUIRED COMPONENTS FitPanel MathMore Spectrum Minuit Minuit2)
+find_dependency(Threads REQUIRED)
 
 # TODO Handle find_dependency(...) for optionally included packages
